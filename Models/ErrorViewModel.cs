823f5ca1414081ad27bb5ed8bd541686b7a097a0
tomaszsrodek99 <98595791+tomaszsrodek99@users.noreply.github.com>
2024-05-08 14:44:52 +0200

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Netwise.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
